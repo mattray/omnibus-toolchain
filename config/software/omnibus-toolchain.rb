0e9cf5285cb90512a4fa371d2ab9492301a88f25
jayashri garud <jgarud@msystechnologies.com>
2021-06-18 14:54:12 +0530

    Testing ruby_cleanup software definition changes
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>

--- config/software/omnibus-toolchain.rb
+++ config/software/omnibus-toolchain.rb
@@ -21,6 +21,7 @@ license :project_license
 
 dependency "nokogiri"
 
+
 # gnu utilities
 if windows?
   dependency "cmake"
@@ -59,4 +60,14 @@ if solaris2? || (freebsd? && ohai["os_version"].to_i < 1000024)
   whitelist_file /libpcrecpp\.so\..+/
 end
 
-dependency "ruby-cleanup"
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # The mini-portile2 gem ships with some test fixture data compressed in a format Apple's notarization
+  # service cannot understand. We need to delete that archive from gem cache to pass notarization.
+  block "Delete test folder of mini-portile2 gem cache so downstream projects pass notarization" do
+    env["VISUAL"] = "echo"
+    gem_install_dir = shellout!("#{install_dir}/embedded/bin/gem environment gemdir", env: env).stdout.chomp
+    dependency "ruby-cleanup"
+  end
+
